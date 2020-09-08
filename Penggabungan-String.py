12514de77fc9cd91d243daa02d6d92964841579f
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 11:39:45 +0700

    Penggabungan-String.py

---
+++ Penggabungan-String.py
@@ -0,0 +1,10 @@
+# Operator + yang kita gunakan untuk perhitungan juga dapat digunakan untuk menggabungkan string.
+# Penggabungan string disebut string concatenation. 
+# Penggabungan string dapat digunakan untuk string dan variable yang mempunyai tipe data string.
+
+
+print('Hello ' + 'Python)        # (+) gabungkan string
+name = 'John'
+print('Nama saya ' + name)       # dapat menggabungkan string dan variable
+# output = Hello Python
+# output = Nama saya John
