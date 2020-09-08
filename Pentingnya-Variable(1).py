35d9ce556b55aa6d09c502662e177675b3fa33f6
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 11:18:06 +0700

    Pentingnya-Variable(1).py

---
+++ Pentingnya-Variable(1).py
@@ -0,0 +1,14 @@
+# code dengan variable 
+
+apple_count = 3                                 # (apple_count) deskriptif
+apple_price = 10                                # (apple_price) deskriptif
+total_price = apple_count * apple_price         # (total_price) deskriptif
+print(total_price)  
+# output = 30
+
+
+# code tanpa variable
+
+total_price = 3 * 10                            # susah untuk mengetahui apa arti dari angka ini
+print(total_price)
+#output = 30
