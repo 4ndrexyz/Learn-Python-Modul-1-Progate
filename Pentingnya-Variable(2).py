5688e572bc1252ddf8c8d4c2588c7c7e4c118693
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 11:24:01 +0700

    Pentingnya-Variable(2).py

---
+++ Pentingnya-Variable(2).py
@@ -0,0 +1,18 @@
+# Kegunaan lain dari variable adalah sebagai berikut:
+# ・Anda dapat menggunakan data yang sama berulang kali.
+# ・Jika Anda ingin mengganti nilai dari variable, Anda hanya perlu menggantinya sekali.
+
+
+# code dengan variable
+
+length = 5
+area = length * length       # (lenght) Dengan menggunakan data yang sama berulang kali
+print(area) 
+# output = 25
+
+
+# code tanpa variable
+
+area = 5 * 5                 # (5 * 5) Butuh untuk menggantikan nilai berulang kali jika mau diubah
+print(area)
+# output = 25
