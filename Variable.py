db6000b69099b63bb79a548b00b757da4e57c779
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 11:07:39 +0700

    Variable.py

---
+++ Variable.py
@@ -0,0 +1,19 @@
+# - Mendefinisikan Variabel
+
+name = 'Jhon'     # (name) nama variable dan ('Jhon') adalah nilai
+number = 24       # (=) tetapkan sebuah nilai
+
+# Untuk menyimpan nilai dalam variable, Anda perlu mendefiniskan variable.
+# Anda dapat melakukannya dengan format berikut: variable_name = nilai. Operator = dalam Python bukan berarti "sama dengan".
+# Operator tersebut digunakan untuk menetapkan nilai ke variable yang berada di sebelah kiri. Nama variable tidak perlu ditutup dengan tanda kutip.
+
+# ---------------------------------
+
+# - Mencetak Variable
+
+name = 'Jhon'
+print(name)    # output = John
+print('name')  # output = name
+
+ # (name) nilai dari variable name
+ # ('name') sebuah string, 'name'
