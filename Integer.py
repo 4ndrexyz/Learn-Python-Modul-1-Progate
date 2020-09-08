963fc999042451dafdbb540c4da3f0d94c483359
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 10:49:44 +0700

    Create Integer.py

---
+++ Integer.py
@@ -0,0 +1,9 @@
+print(3) # output = 3
+print(3 + 7) # output = 10  
+print(7 - 3) # output = 4
+
+# jangan apit nomor dengan kutip ('')
+
+# Perbedaan String dan Integer
+# String dan integer diinterpretasikan berbeda dalam pemrograman.
+# Seperti kode di atas, 3 + 5 akan menghasilkan 8, hasil penambahannya. Akan tetapi, jika Anda memakai tanda kutip dan membuatnya tipe data string, hasilnya akan menjadi 3 + 5.
