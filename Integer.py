47488a89991a811e83253d3b8ba7fdadaa862050
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 10:57:30 +0700

    Update Integer.py

--- Integer.py
+++ Integer.py
@@ -1,9 +1,8 @@
 print(3) # output = 3
 print(3 + 7) # output = 10  
 print(7 - 3) # output = 4
+print(3 * 1) # output = 21
+print(3 / 2) # output = 1.5 (nomor desimal)
+print(7 % 3) # output = 1 (sisa dari pembagian)
 
 # jangan apit nomor dengan kutip ('')
-
-# Perbedaan String dan Integer
-# String dan integer diinterpretasikan berbeda dalam pemrograman.
-# Seperti kode di atas, 3 + 5 akan menghasilkan 8, hasil penambahannya. Akan tetapi, jika Anda memakai tanda kutip dan membuatnya tipe data string, hasilnya akan menjadi 3 + 5.
