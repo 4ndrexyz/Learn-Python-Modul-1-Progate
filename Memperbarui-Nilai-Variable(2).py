896f2c988ce774f243efcb2ae31fea2c16b6a767
Andre Firmansah <63025746+andrefirmansah@users.noreply.github.com>
2020-09-08 11:32:03 +0700

    Memperbarui-Nilai-Variable(2).py

---
+++ Memperbarui-Nilai-Variable(2).py
@@ -0,0 +1,11 @@
+# Jika Anda ingin menambahkan nilai sebuah variable, Anda dapat menetapkan jumlah dari variable tersebut dan angka ke variable itu sendiri. 
+# Ini terlihat aneh, namun perlu diingat bahwa = berarti menetapkan, bukan sama dengan.
+
+
+# Tetapkan variable x
+x = 5
+print(x)
+x = x + 5             # tambahkan 3 ke variable x
+print(x)
+# output = 5
+# output = 8          <- nilai dari variable x diperbarui ke 8
